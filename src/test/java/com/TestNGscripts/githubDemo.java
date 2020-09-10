034079af5892279141f725c307743fa3486a4202
taranpreet22 <taramkaur2287@gmail.com>
2020-09-10 08:26:26 -0500

    Signed-off-by: taranpreet22 <taramkaur2287@gmail.com>

---
+++ src/test/java/com/TestNGscripts/githubDemo.java
@@ -0,0 +1,17 @@
+package com.TestNGscripts;
+
+import org.junit.Test;
+
+public class githubDemo {
+
+	@Test
+	
+public void login()
+
+{
+	System.out.println("login into functionality");
+	
+	
+	
+}
+}
